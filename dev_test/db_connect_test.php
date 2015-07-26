d57bd7c5bf0078b8ff477c49ce65702cad7927a7
Alex Holovin <dvjmixxxer@gmail.com>
2015-07-26 18:49:42 +0300

    DB test updated

--- dev_test/db_connect_test.php
+++ dev_test/db_connect_test.php
@@ -1,10 +1,10 @@
 <?php
-    include '../PSDO/Config/config.php';
+    include '../PSDO/Config/Config.php';
 
     try {
         $db = new PDO('mysql:host='.PSDO_CFG_DB_HOST.';'.'dbname='.PSDO_CFG_DB_NAME, PSDO_CFG_DB_USER, PSDO_CFG_DB_PASS);
 
-        $db = $db->prepare("INSERT INTO test (testvalue) VALUES (Maka)");
+        $db = $db->prepare("INSERT INTO test (testvalue) VALUES ('ddd')");
         $db->execute();
         print "Seems ok";
         print __DIR__;
