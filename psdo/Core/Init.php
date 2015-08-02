0a3a2aa1d23ec4efd743ace892810cbdaf0e4479
Alex Holovin <dvjmixxxer@gmail.com>
2015-08-03 01:00:45 +0300

    Global config moved to init

--- psdo/Core/Init.php
+++ psdo/Core/Init.php
@@ -10,3 +10,13 @@
     $loader = new AutoLoader();
     $loader->register();
     $loader->addNamespace('PSDO', $_SERVER['DOCUMENT_ROOT'].'PSDO');
+
+    // const
+    define('PSDO_ENVIRONMENT', 'dev');
+    define('PSDO_ROOT_DIR', $_SERVER['DOCUMENT_ROOT'].'PSDO/');
+
+    // dev
+    if (PSDO_ENVIRONMENT == 'dev') {
+        error_reporting(E_ALL);
+        ini_set("display_errors", 1);
+    }
